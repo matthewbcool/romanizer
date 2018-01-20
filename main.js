238ed9e9c8efeb984e032f8c9bdb5a94974bd58b
Matt Cool <matthewbcool@gmail.com>
2018-01-20 14:52:22 -0600

    added sound effect with howl

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
 const $clouds = $('.cloud1, .cloud2, .cloud3, .cloud4'); 
+
+//add Howl with stop at the end of fade
+const chisle = new Howl({
+    src: ['http://www.freesfx.co.uk/rx2/mp3s/2/14070_1459886694.mp3'],
+    onfade: function stopSound() {
+        chisle.stop();
+    },
+  });
+
+//Shake function
 jQuery.fn.shake = function() {
   this.each(function(i) {
       $(this).css({
@@ -26,8 +36,11 @@ jQuery.fn.shake = function() {
   return this;
 }
 
+//hide the clouds
+
 $clouds.hide();
 
+//numeral conversion algorithm
 function toRoman(num) {  
     var result = '';
     var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
@@ -41,12 +54,15 @@ function toRoman(num) {
     return result;
   }
   
+//Jquery animation and enter key function
   $ ( '#romanize' ).click(function(){
+    chisle.play();
     $( '#output' ).empty();
     $( '#output' ).append(toRoman($('#input-number').val()));
     $clouds.show().shake().fadeOut();
     $( '#input-number' ).val("")
-
+    chisle.fade(1.0, 0.0, 2000);
+    
   });
   
   $('#input-number').keyup(function(event){
@@ -54,8 +70,4 @@ function toRoman(num) {
       $('#romanize').click();
     }
   });
-
-
-
-
 });
