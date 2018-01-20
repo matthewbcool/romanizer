151c532dff7ab4f9051183d5c2c1d0e4d40c0e99
Matt Cool <matthewbcool@gmail.com>
2018-01-20 00:32:46 -0600

    added animation with jquery

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
 $( document ).ready(function() {
+const $clouds = $('.cloud1, .cloud2, .cloud3, .cloud4'); 
+jQuery.fn.shake = function() {
+  this.each(function(i) {
+      $(this).css({
+          "position": "absolute"
+      });
+      for (var x = 1; x <= 3; x++) {
+          $(this).animate({
+              left: 43
+          }, 10).animate({
+              left: 23
+          }, 50).animate({
+              left: 23
+          }, 10).animate({
+              left: 13
+          }, 50).animate({
+              left: 43
+          }, 50).animate({
+              left: 33
+          }, 50).animate({
+              left: 43
+          }, 50);
+      }
+  });
+  return this;
+}
+
+$clouds.hide();
+
 function toRoman(num) {  
     var result = '';
     var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
@@ -15,6 +44,8 @@ function toRoman(num) {
   $ ( '#romanize' ).click(function(){
     $( '#output' ).empty();
     $( '#output' ).append(toRoman($('#input-number').val()));
+    $clouds.show().shake();
+
   });
   
   $('#input-number').keyup(function(event){
@@ -22,4 +53,8 @@ function toRoman(num) {
       $('#romanize').click();
     }
   });
+
+
+
+
 });
